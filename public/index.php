9fb993b0ecda9017012cbfeb8024b7f7f3a50acf
Your Name <njniran456@gmail.com>
2026-06-10 20:19:10 +0530

    updated

--- public/index.php
+++ public/index.php
@@ -22,6 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 // Run CSRF check
 CsrfMiddleware::handle();
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 
 // Load routes
 require_once __DIR__ . '/../app/Routes/api.php';
